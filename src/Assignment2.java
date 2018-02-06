ff2c4c6e6c816ab1c5adde61bc2f1f2406b33dff
Radiodrome <mbrown44@missouriwestern.edu>
2018-02-05 18:55:00 -0600

    added user input and Places class

--- src/Assignment2.java
+++ src/Assignment2.java
@@ -1,31 +1,35 @@
-import com.mysql.jdbc.Statement;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.Scanner;
 
 /**
  * Created by brown on 2/4/2018.
  */
 public class Assignment2 {
+    static Connection conn;
+    static Statement st;
+    static ResultSet rs;
     public static void main(String[] args){
         String host = "jdbc:mysql://turing.cs.missouriwestern.edu/misc";
         String user = "csc254";
         String password = "age126";
 
-        Connection conn;
-        Statement st;
-        ResultSet rs;
+        Scanner reader = new Scanner(System.in);  // Reading from System.in
+        System.out.println("Enter a zipcode: ");
+        int z = reader.nextInt(); // Scans the next token of the input as an int.
+        System.out.println("Enter a radius: ");
+        int r = reader.nextInt();
+        //once finished
+        reader.close();
 
         try {
             conn = DriverManager.getConnection(host,user,password);
-            String query = "select * from zips";
-            st = (Statement) conn.createStatement();
+            String query = String.format("select zipcode, city, state, lat, `long`, estimatedpopulation from zips2 WHERE (zipcode > %d)", z);
+            st = conn.createStatement();
             rs = st.executeQuery(query);
 
-            while(rs.next()){
-                System.out.println(rs.getString("zip_code"));
-            }
+            ResultSetMetaData rsMD = rs.getMetaData();
+            int colCount = rsMD.getColumnCount();
+
             conn.close();
         } catch (SQLException e) {
             e.printStackTrace();
--- src/Place.java
+++ src/Place.java
@@ -1,7 +1,91 @@
-package PACKAGE_NAME;
-
 /**
  * Created by brown on 2/5/2018.
  */
 public class Place {
+    int zipcode;
+    String city;
+    String state;
+    double lat;
+    double lon;
+    int pop;
+    double distFromCenter;
+
+    public Place(int zipcode, String city, String state, double lat, double lon, int pop) {
+        this.zipcode = zipcode;
+        this.city = city;
+        this.state = state;
+        this.lat = lat;
+        this.lon = lon;
+        this.pop = pop;
+        distFromCenter = -1.0;
+    }
+
+    public int getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(int zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    public double getLon() {
+        return lon;
+    }
+
+    public void setLon(double lon) {
+        this.lon = lon;
+    }
+
+    public int getPop() {
+        return pop;
+    }
+
+    public void setPop(int pop) {
+        this.pop = pop;
+    }
+
+    public double getDistFromCenter() {
+        return distFromCenter;
+    }
+
+    public void setDistFromCenter(double distFromCenter) {
+        this.distFromCenter = distFromCenter;
+    }
+
+    @Override
+    public String toString() {
+        return "Place{" +
+                "zipcode=" + zipcode +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", lat=" + lat +
+                ", lon=" + lon +
+                ", pop=" + pop +
+                ", distFromCenter=" + distFromCenter +
+                '}';
+    }
 }
